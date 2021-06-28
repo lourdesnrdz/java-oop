f1b0d8df2cee6d58f82dfd1216b5985ef3c5c249
lourdesnrdz <lourdesnrdz@gmail.com>
2021-06-28 10:38:48 -0500

    overriding method toString

--- MyMedicalAppointments/src/User.java
+++ MyMedicalAppointments/src/User.java
@@ -53,4 +53,17 @@ public class User {
             this.phoneNumber = phoneNumber;
         }
     }
+
+    @Override
+    public String toString() {
+//        return "User{" +
+//                "id=" + id +
+//                ", name='" + name + '\'' +
+//                ", email='" + email + '\'' +
+//                ", address='" + address + '\'' +
+//                ", phoneNumber='" + phoneNumber + '\'' +
+//                '}';
+        return "User: " + name + ", Email: " + email +
+                "\nAddress: " + address + ". Phone:  " + phoneNumber;
+    }
 }
