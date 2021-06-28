daf139ea2c9ec992ceb1c9c2f184dd14a7810479
lourdesnrdz <lourdesnrdz@gmail.com>
2021-06-28 15:33:01 -0500

    anonymous classes

--- MyMedicalAppointments/src/model/Main.java
+++ MyMedicalAppointments/src/model/Main.java
@@ -20,6 +20,24 @@ public class Main {
         User userPa = new Patient("Anahi", "ana@ana.com");
         userPa.showDataUser();
 
+        User user1 = new User("Anahi", "ana@ana.com") {
+            @Override
+            public void showDataUser() {
+                System.out.println("Doctor");
+                System.out.println("Hospital: Cruz Verde");
+                System.out.println("Departamento: Geriatria");
+            }
+        };
+
+        user1.showDataUser();
+
+        ISchedulable iSchedulable = new ISchedulable() {
+            @Override
+            public void schedule(Date date, String time) {
+
+            }
+        };
+
 //        for (model.Doctor.AvailableAppointment aA: myDoctor.getAvailableAppointments()) {
 //            System.out.println(aA.getDate() + " " + aA.getTime());
 //        }
